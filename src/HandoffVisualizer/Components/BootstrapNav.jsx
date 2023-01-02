8a813322b22bae4522c54f4966b0c976bdc4df9c
Pratul30 <pratul.rocks7777@gmail.com>
2023-01-03 00:39:36 +0530

    handoff visualizer

---
+++ src/HandoffVisualizer/Components/BootstrapNav.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import { Navbar, Nav } from "react-bootstrap";
+import "./BootstrapNav.css";
+
+export default class BootstrapNav extends Component {
+    render() {
+        return (
+            <div>
+                <Navbar
+                    bg="dark"
+                    expand="md"
+                    variant="dark"
+                    fixed="top"
+                    className="navbrand"
+                >
+                    <Navbar.Brand href="#home">
+            
+                        Wireless Network Handoff
+                    </Navbar.Brand>
+                    
+                    
+                </Navbar>
+            </div>
+        );
+    }
+}
