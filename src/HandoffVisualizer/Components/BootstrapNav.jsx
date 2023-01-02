ca1c1ad6c5d107c03e1233c94b9f62667049f0bd
Pratul30 <pratul.rocks7777@gmail.com>
2023-01-03 00:58:35 +0530

    build0

--- src/HandoffVisualizer/Components/BootstrapNav.jsx
+++ src/HandoffVisualizer/Components/BootstrapNav.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Navbar, Nav } from "react-bootstrap";
+import { Navbar } from "react-bootstrap";
 import "./BootstrapNav.css";
 
 export default class BootstrapNav extends Component {
